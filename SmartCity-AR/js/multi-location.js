7dc5cb7a779f263a72ec1af540366f6d44a7ff3c
csapot <csapot@tmit.bme.hu>
2020-04-07 14:41:57 +0200

    t9

--- SmartCity-AR/js/multi-location.js
+++ SmartCity-AR/js/multi-location.js
@@ -8,11 +8,28 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: 'Magnemite',
+            name: 'Magnemite, Lv. 5, HP 10/10',
             location: {
-                lat: 48.105516826902914,
-                lng: 20.631016913739035,
-            }
+                lat: 48.10568369386877,
+                lng: 20.630873655174184,
+            },
+			gtlf: './assets/magnemite/scene.gltf'
+        },
+		{
+            name: 'Articuno, Lv. 80, HP 100/100',
+            location: {
+                lat: 48.105621006434774,
+                lng: 20.631084208581854,
+            },
+			gtlf: './assets/articuno/scene.gltf'
+        },
+		{
+            name: 'Dragonite, Lv. 99, HP 150/150',
+            location: {
+                lat: 48.10558339393768,
+                lng: 20.631230388973165,
+            },
+			gtlf: './assets/dragonite/scene.gltf'
         },
     ];
 }
@@ -26,7 +43,7 @@ function renderPlaces(places) {
 
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
+        model.setAttribute('gltf-model', place.gltf);
         model.setAttribute('rotation', '0 180 0');
         model.setAttribute('animation-mixer', '');
         model.setAttribute('scale', '0.5 0.5 0.5');
